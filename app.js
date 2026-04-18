b51bb4cba881f7973ad1192a09a298458880e315
Rohan <rohan.22310698@viit.ac.in>
2026-04-18 13:08:38 +0530

    test cicd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World 🚀");
+    res.send("CI/CD Working 🚀");
 });
 
 app.listen(3000, () => {
